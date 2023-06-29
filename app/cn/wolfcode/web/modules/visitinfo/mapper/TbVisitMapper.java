e610ebb5c83b0a9314fbfc24be737e04d493885a
sudashui118 <126454147+sudashui118@users.noreply.github.com>
2023-06-29 14:55:56 +0800

    Add files via upload

---
+++ app/cn/wolfcode/web/modules/visitinfo/mapper/TbVisitMapper.java
@@ -0,0 +1,16 @@
+package cn.wolfcode.web.modules.visitinfo.mapper;
+
+import cn.wolfcode.web.modules.visitinfo.entity.TbVisit;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 拜访信息表 Mapper 接口
+ * </p>
+ *
+ * @author 苏打水
+ * @since 2023-06-29
+ */
+public interface TbVisitMapper extends BaseMapper<TbVisit> {
+
+}
